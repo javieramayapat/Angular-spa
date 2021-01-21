99408feffd2507126117b4107e206448d45362db
Javier Amaya <javieramayapat@gmail.com>
2021-01-20 20:56:49 -0600

    RouterLink y RouterLinkActive

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 import { HomeComponent } from './components/home/home.component';
 
 const ROUTES: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'heroes', component: HeroesComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
