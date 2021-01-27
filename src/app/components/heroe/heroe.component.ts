361cd730cde6b0819b3d6793391c1eb4bdd819e1
Javier Amaya <javieramayapat@gmail.com>
2021-01-26 20:58:50 -0600

    Modification of the service to bring a hero according to a parameter of the url

--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService } from 'src/app/Servicios/heroes.service';
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
   styleUrls: ['./heroe.component.css']
 })
-export class HeroeComponent implements OnInit {
+export class HeroeComponent{
 
-  constructor() { }
+  heroe:any = {};
 
-  ngOnInit(): void {
+  constructor(private activatedRoute: ActivatedRoute, private heroesService: HeroesService) { 
+    this.activatedRoute.params.subscribe( params => {
+      console.log(params['id']);
+      this.heroe = this.heroesService.getHeroe(params['id']);
+      console.log(this.heroe);
+      
+      
+    })
   }
 
 }
